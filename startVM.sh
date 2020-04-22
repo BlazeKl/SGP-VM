044a151d3d03c4f0e6b0e94a65fb5533f8a35097
BlazeKl <felipe.perezc@utem.cl>
2020-04-22 17:05:54 -0400

    Update

--- startVM.sh
+++ startVM.sh
@@ -16,7 +16,8 @@ echo -n "1002 aaf0" > /sys/bus/pci/drivers/vfio-pci/new_id
 qemu-system-x86_64 \
     -runas pipe \
     -nographic -vga none -parallel none -serial none \
-    -enable-kvm -M q35 -m 8192 -cpu host -smp 8,sockets=1,cores=4,threads=2 \
+    -enable-kvm -M q35 -m 8192 -cpu Westmere-IBRS,vme=on,pclmulqdq=on,pcid=on,x2apic=on,tsc-deadline=on,hypervisor=on,arat=on,tsc-adjust=on,umip=on,stibp=on,arch-capabilities=on,ssbd=on,pdpe1gb=on,rdtscp=on,ibpb=on,amd-ssbd=on,hv-time,hv-relaxed,hv-vapic,hv-spinlocks=0x1fff \
+    -smp 10,sockets=10,cores=1,threads=1 \
     -bios /usr/share/qemu/bios.bin -vga none \
     -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
     -device pcie-root-port,port=0x10,chassis=2,id=pci.1,bus=pcie.0,multifunction=on,addr=0x2 \
@@ -32,9 +33,23 @@ qemu-system-x86_64 \
     -device vfio-pci,host=03:00.1,bus=pcie.0 \
     -device virtio-net,netdev=vmnic -netdev user,id=vmnic \
     -hda /dev/sdc \
-#   -drive media=cdrom,file=$files/virtio.iso,id=cd1,if=none \
-#   -device ide-cd,bus=ide.1,drive=cd1 \
     -device usb-host,hostbus=6,hostaddr=2,id=hostdev0,bus=usb.0,port=1 \
     -device usb-host,hostbus=6,hostaddr=3,id=hostdev1,bus=usb.0,port=2 \
     -device usb-host,hostbus=8,hostaddr=2,id=hostdev2,bus=usb.0,port=3 
+    
+#   -drive media=cdrom,file=$files/virtio.iso,id=cd1,if=none \
+#   -device ide-cd,bus=ide.1,drive=cd1 \
+    
+echo -n "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
+echo -n "0000:03:00.1" > /sys/bus/pci/drivers/vfio-pci/unbind
+
+echo -n 1002 67df > /sys/bus/pci/drivers/vfio-pci/remove_id
+echo -n 1002 aaf0 > /sys/bus/pci/drivers/vfio-pci/remove_id
+
+modprobe -r vfio-pci
+
+echo -n "0000:03:00.1" > /sys/bus/pci/drivers/snd_hda_intel/bind
+echo -n "0000:03:00.0" > /sys/bus/pci/drivers/amdgpu/bind
+
+systemctl start sddm
 
