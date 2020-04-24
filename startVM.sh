841b1a80db96097e9942162bbe14eb2ef417f668
BlazeKl <felipe.perezc@utem.cl>
2020-04-23 22:11:41 -0400

    More devices

--- startVM.sh
+++ startVM.sh
@@ -19,7 +19,7 @@ echo -n "1002 aaf0" > /sys/bus/pci/drivers/vfio-pci/new_id
 qemu-system-x86_64 \
     -runas vm \
     -nographic -vga none -parallel none -serial none \
-    -enable-kvm -M q35 -m 8192 -cpu host -smp 12,sockets=1,cores=6,threads=2 \
+    -enable-kvm -M q35 -m 10240 -cpu Westmere-IBRS,vme=on,pclmulqdq=on,vmx=on,pcid=on,x2apic=on,tsc-deadline=on,hypervisor=on,arat=on,tsc-adjust=on,umip=on,stibp=on,arch-capabilities=on,ssbd=on,pdpe1gb=on,rdtscp=on,ibpb=on,amd-ssbd=on,hv-time,hv-relaxed,hv-vapic,hv-spinlocks=0x1fff -smp 10,sockets=1,cores=5,threads=2 \
     -bios /usr/share/qemu/bios.bin -vga none \
     -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
     -device pcie-root-port,port=0x10,chassis=2,id=pci.1,bus=pcie.0,multifunction=on,addr=0x2 \
@@ -38,7 +38,8 @@ qemu-system-x86_64 \
     -hdb /dev/sda1 \
     -device usb-host,hostbus=6,hostaddr=2,id=hostdev0,bus=usb.0,port=1 \
     -device usb-host,hostbus=6,hostaddr=3,id=hostdev1,bus=usb.0,port=2 \
-    -device usb-host,hostbus=8,hostaddr=2,id=hostdev2,bus=usb.0,port=3 
+    -device usb-host,hostbus=8,hostaddr=2,id=hostdev2,bus=usb.0,port=3 \
+    -device usb-host,hostbus=7,hostaddr=2,id=hostdev3,bus=usb.0,port=4
 
 echo -n "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
 echo -n "0000:03:00.1" > /sys/bus/pci/drivers/vfio-pci/unbind
