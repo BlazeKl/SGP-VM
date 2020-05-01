39f89ff8b3001361323a01a08da32e24af6084e8
BlazeKl <felipe.perezc@utem.cl>
2020-05-01 13:19:51 -0400

    Search USB devices

--- startVM.sh
+++ startVM.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 files=.
+headset='1038:1228'
+mouse='1532:0020'
+keyboard='1a2c:427c'
+audiousb='1b3f:2008'
+opt1='28de:1142'
+
+bus1=$(lsusb | grep -oE "[1-9].{0,17}$headset.{0,0}" | cut -c 1)
+id1=$(lsusb | grep -oE "[1-9].{0,6}$headset.{0,0}" | cut -c 1)
+
+bus2=$(lsusb | grep -oE "[1-9].{0,17}$mouse.{0,0}" | cut -c 1)
+id2=$(lsusb | grep -oE "[1-9].{0,6}$mouse.{0,0}" | cut -c 1)
+
+bus3=$(lsusb | grep -oE "[1-9].{0,17}$keyboard.{0,0}" | cut -c 1)
+id3=$(lsusb | grep -oE "[1-9].{0,6}$keyboard.{0,0}" | cut -c 1)
+
+bus4=$(lsusb | grep -oE "[1-9].{0,17}$audiousb.{0,0}" | cut -c 1)
+id4=$(lsusb | grep -oE "[1-9].{0,6}$audiousb.{0,0}" | cut -c 1)
+
+bus5=$(lsusb | grep -oE "[1-9].{0,17}$opt1.{0,0}" | cut -c 1)
+id5=$(lsusb | grep -oE "[1-9].{0,6}$opt1.{0,0}" | cut -c 1)
 
 pkill -9 -u pipe
 systemctl stop sddm
@@ -19,7 +39,7 @@ echo -n "1002 aaf0" > /sys/bus/pci/drivers/vfio-pci/new_id
 qemu-system-x86_64 \
     -runas vm \
     -nographic -vga none -parallel none -serial none \
-    -enable-kvm -M q35 -m 8192 -cpu host,hv_time,kvm=off,hv_vendor_id=null,-hypervisor -smp 10,sockets=1,cores=5,threads=2 \
+    -enable-kvm -M q35 -m 8192 -watchdog ib700 -cpu host,hv_relaxed,hv_time,kvm=off,hv_vendor_id=null,-hypervisor -smp 10,sockets=1,cores=5,threads=2 \
     -bios /usr/share/qemu/bios.bin -vga none \
     -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
     -device pcie-root-port,port=0x10,chassis=2,id=pci.1,bus=pcie.0,multifunction=on,addr=0x2 \
@@ -34,12 +54,15 @@ qemu-system-x86_64 \
     -device vfio-pci,host=03:00.0,bus=root.1,addr=00.0,multifunction=on,x-vga=on,romfile=$files/Ellesmere.rom \
     -device vfio-pci,host=03:00.1,bus=pcie.0 \
     -device virtio-net,netdev=vmnic -netdev user,id=vmnic \
-    -hda /dev/sdc \
-    -hdb /dev/sda1 \
-    -device usb-host,hostbus=6,hostaddr=2,id=hostdev0,bus=usb.0,port=1 \
-    -device usb-host,hostbus=6,hostaddr=3,id=hostdev1,bus=usb.0,port=2 \
-    -device usb-host,hostbus=8,hostaddr=2,id=hostdev2,bus=usb.0,port=3 \
-    -device usb-host,hostbus=7,hostaddr=2,id=hostdev3,bus=usb.0,port=4
+    -drive file=/dev/sdc,format=raw,cache=writeback,if=virtio \
+    -drive file=/dev/sda1,format=raw,cache=writeback,if=virtio \
+    -drive media=cdrom,file=$files/virtio.iso,id=cd1,if=none \
+    -device ide-cd,bus=ide.1,drive=cd1 \
+    -device usb-host,hostbus="$bus1",hostaddr="$id1",id=hostdev0,bus=usb.0,port=1 \
+    -device usb-host,hostbus="$bus2",hostaddr="$id2",id=hostdev1,bus=usb.0,port=2 \
+    -device usb-host,hostbus="$bus3",hostaddr="$id3",id=hostdev2,bus=usb.0,port=3 \
+    -device usb-host,hostbus="$bus4",hostaddr="$id4",id=hostdev3,bus=usb.0,port=4 \
+    -device usb-host,hostbus="$bus5",hostaddr="$id5",id=hostdev4,bus=usb.0,port=5
 
 echo -n "0000:03:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
 echo -n "0000:03:00.1" > /sys/bus/pci/drivers/vfio-pci/unbind
