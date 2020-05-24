b2e02cdd626eac3a76fcb766a5773a7102072841
BlazeKl <felipe.perezc@utem.cl>
2020-05-23 20:17:20 -0400

    Clean

--- disablegpu.bat
+++
@@ -1,3 +0,0 @@
-devcon64.exe disable "PCI\VEN_1002&DEV_67DF*"
-
-devcon64.exe disable "HDAUDIO\FUNC_01&VEN_1002&DEV_AA01*"
--- enablegpu.bat
+++
@@ -1,3 +0,0 @@
-devcon64.exe enable "PCI\VEN_1002&DEV_67DF*"
-
-devcon64.exe enable "HDAUDIO\FUNC_01&VEN_1002&DEV_AA01*"
