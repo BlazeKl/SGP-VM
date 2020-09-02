af64c6402b96d67125552205b0c60aeb7ffb0014
BlazeKl <felipe.perezc@utem.cl>
2020-09-02 17:53:50 -0400

    preallocate ram

--- startVM.sh
+++ startVM.sh
@@ -37,7 +37,7 @@ if [ "$_is_osx" == "true" ]; then
     -runas vm 
     -nographic -vga none -parallel none -serial none 
     -enable-kvm 
-    -m $RAM 
+    -m $RAM -mem-prealloc
     -machine q35,accel=kvm 
     -smp $(( $CORES * $THREADS )),cores=$CORES,threads=$THREADS,sockets=1 
     -cpu Penryn,vendor=GenuineIntel,kvm=on,rdtscp=off,+sse3,+sse4.2,+aes,+invtsc 
@@ -61,7 +61,7 @@ else
     start_VM="qemu-system-x86_64 
     -runas vm 
     -nographic -vga none -parallel none -serial none 
-    -enable-kvm -M q35 -m $RAM 
+    -enable-kvm -M q35 -m $RAM -mem-prealloc -no-hpet
     -cpu host,hv_relaxed,hv_time,kvm=off,rdtscp=off,hv_vendor_id=null,-hypervisor -smp $(( $CORES * $THREADS )),sockets=1,cores=$CORES,threads=$THREADS 
     -bios /usr/share/qemu/bios.bin -vga none 
     -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 
