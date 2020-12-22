399b01224fb245ea779178f945db7fdcc8d76dcf
BlazeKl <felipe.perezc@utem.cl>
2020-12-22 14:51:58 -0300

    tweaks

--- startVM.sh
+++ startVM.sh
@@ -68,7 +68,7 @@ fi
 
 #Kill Host display
 if [ "$_exit_display" == "true" ]; then
-    systemctl isolate multi-user.target
+    systemctl stop display-manager.service
     sleep 5
     echo -n "0" > /sys/class/vtconsole/vtcon0/bind
     echo -n "0" > /sys/class/vtconsole/vtcon1/bind
@@ -137,6 +137,5 @@ fi
 #Start display manager if killed
 if [ "$_exit_display" == "true" ]; then
     echo -n "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
-    sleep 5
-    systemctl isolate graphical.target
+    systemctl start display-manager.service
 fi
