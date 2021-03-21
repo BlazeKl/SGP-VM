6d75e541f6f291e086df26726c138355da01d124
BlazeKl <felipe.perezc@utem.cl>
2021-03-21 00:43:56 -0300

    re-enable memory preallocation and debug threads

--- startVM.sh
+++ startVM.sh
@@ -32,9 +32,10 @@ else
 fi
 
 #Set QEMU arguments, modified later in the script to add devices
-args="-runas $_current_user
+args="-name sgpvm,debug-threads=on
+    -runas $_current_user
     -nographic -vga none -parallel none -serial none -nodefaults
-    -enable-kvm -M q35 -m $_RAM -no-hpet
+    -enable-kvm -M q35 -m $_RAM -mem-prealloc -no-hpet
     -cpu $_CPU
     -smp $(( $_CORES * $_THREADS )),sockets=1,cores=$_CORES,threads=$_THREADS
     -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_CODE.fd
