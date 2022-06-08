b9ed42fc6e3cd261e441ad2f177b6db8c362448f
BlazeKl <felipe.perezc@utem.cl>
2022-06-08 14:32:19 -0400

    random stuff

--- startVM.sh
+++ startVM.sh
@@ -32,15 +32,31 @@ else
 fi
 
 #Set QEMU arguments, modified later in the script to add devices
-args="-name sgpvm,debug-threads=on
+if [ "$_ovmf" == "true" ]; then
+    args="-name sgpvm,debug-threads=on
     -runas $_current_user
-    -nographic -vga none -parallel none -serial none -nodefaults
+    -vga none -nodefaults
     -enable-kvm -M q35 -m $_RAM -mem-prealloc -no-hpet
     -cpu $_CPU
     -smp $(( $_CORES * $_THREADS )),sockets=1,cores=$_CORES,threads=$_THREADS
+    -rtc base=localtime,driftfix=slew
+    -global kvm-pit.lost_tick_policy=delay
     -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_CODE.fd
-    -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1
+    -device pcie-root-port,id=pcie.1,bus=pcie.0,addr=1c.0,slot=1,chassis=1,multifunction=on
     -device virtio-net,netdev=vmnic -netdev user,id=vmnic "
+else
+    args="-name sgpvm,debug-threads=on
+    -runas $_current_user
+    -vga none -nodefaults
+    -enable-kvm -M q35 -m $_RAM -mem-prealloc -no-hpet
+    -cpu $_CPU
+    -smp $(( $_CORES * $_THREADS )),sockets=1,cores=$_CORES,threads=$_THREADS
+    -rtc base=localtime,driftfix=slew
+    -global kvm-pit.lost_tick_policy=delay
+    -bios /usr/share/qemu/bios.bin
+    -device pcie-root-port,id=pcie.1,bus=pcie.0,addr=1c.0,slot=1,chassis=1,multifunction=on
+    -device virtio-net,netdev=vmnic -netdev user,id=vmnic "
+fi
 
 #Add config file arguments
 args+="$_ext_parameters "
@@ -49,8 +65,13 @@ args+="$_ext_parameters "
 GPUIOMMU=$(get_iommu $GPUID)
 HDMIOMMU=$(get_iommu $HDMID)
 if [ -n "$GPUIOMMU" ] && [ -n "$HDMIOMMU" ]; then 
-    args+="-device vfio-pci,host=$GPUIOMMU,bus=root.1,addr=00.0,multifunction=on,romfile=$_vbios
-    -device vfio-pci,host=$HDMIOMMU,bus=root.1,addr=00.1 "
+    if [ "$_use_vbios" == "true" ]; then
+        args+="-device vfio-pci,host=$GPUIOMMU,bus=pcie.1,addr=00.0,multifunction=on,x-vga=on,romfile=$_vbios
+        -device vfio-pci,host=$HDMIOMMU,bus=pcie.1,addr=00.1 "
+    else
+        args+="-device vfio-pci,host=$GPUIOMMU,bus=pcie.1,addr=00.0,multifunction=on,x-vga=on
+        -device vfio-pci,host=$HDMIOMMU,bus=pcie.1,addr=00.1 "
+    fi
 else
     echo "[$GPUID/$HDMID]GPU not found, exiting"
     exit 1
@@ -60,7 +81,7 @@ if [ "$_pci_devices" == "true" ]; then
     for n in "${PCIID[@]}"; do
         PCIOMMU=$(get_iommu $n)
         if [ -n "$PCIOMMU" ]; then
-            args+="-device vfio-pci,host=$PCIOMMU,bus=root.1 "
+            args+="-device vfio-pci,host=$PCIOMMU,bus=pcie.1 "
         else
             echo "[$n]Device not found"
         fi
@@ -69,15 +90,26 @@ fi
 
 #Kill Host display
 if [ "$_exit_display" == "true" ]; then
+    echo "Exiting X11 session"
+    killall klauncher kwin latte-dock plasmashell
     systemctl stop $_display_manager.service
+    systemctl stop systemd-logind.service
+    pkill -9 x
     if [ "$_display_manager" == "gdm" ]; then
         killall gdm-x-session
     fi
-    echo -n "0" > /sys/class/vtconsole/vtcon0/bind
-    echo -n "0" > /sys/class/vtconsole/vtcon1/bind
-    echo -n "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/unbind
+    echo "Waiting (1)..."
+    sleep 5
+    echo 0 > /sys/class/vtconsole/vtcon0/bind
+    echo 0 > /sys/class/vtconsole/vtcon1/bind
+    echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+    echo "Waiting (2)..."
     sleep 5
 fi
+
+#Services
+systemctl start sshd.service
+systemctl start smb.service
 
 #Add USB Devices
 if [ "$_usb_devices" == "true" ]; then
@@ -96,20 +128,22 @@ if [ "$_usb_devices" == "true" ]; then
 fi
 
 #Bind PCI Devices to vfio-pci if needed
-modprobe vfio-pci
+
 GPUKM1=$(get_kmodule $GPUIOMMU)
 GPUKM2=$(get_kmodule $HDMIOMMU)
 
 if [ "$GPUKM1" != "vfio-pci" ] && [ "$GPUKM2" != "vfio-pci" ]; then
+    virsh nodedev-detach pci_0000_${GPUIOMMU//[:.]/_}
+    virsh nodedev-detach pci_0000_${HDMIOMMU//[:.]/_}
     if [ "$GPUKM1" == "nvidia" ]; then
         echo "Nvidia driver detected, removing modules..."
+        #lsof /dev/nvidia0 | grep mem
         modprobe -r nvidia_drm
         modprobe -r nvidia_modeset
         modprobe -r nvidia_uvm
         modprobe -r nvidia
     fi
-    virsh nodedev-detach pci_0000_${GPUIOMMU//[:.]/_}
-    virsh nodedev-detach pci_0000_${HDMIOMMU//[:.]/_}
+    modprobe vfio-pci
 fi
 
 if [ "$_pci_devices" == "true" ]; then
@@ -158,6 +192,9 @@ fi
 if [ "$_exit_display" == "true" ]; then
     echo 1 > /sys/class/vtconsole/vtcon0/bind
     echo 1 > /sys/class/vtconsole/vtcon1/bind
+    if [ "$GPUKM1" == "nvidia" ]; then
+        nvidia-xconfig --query-gpu-info > /dev/null 2>&1
+    fi
     echo -n "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
     systemctl isolate graphical.target
 fi
